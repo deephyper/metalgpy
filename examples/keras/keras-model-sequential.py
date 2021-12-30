acf1459aa70bc3cc83d7baa35baa96547e2d252b
Deathn0t <romainegele@gmail.com>
2021-12-30 11:53:23 -0500

    adding example for keras model sequential

---
+++ examples/keras/keras-model-sequential.py
@@ -0,0 +1,31 @@
+import tensorflow as tf
+import metalgpy as mpy
+import numpy as np
+
+# meta symbols
+Sequential = mpy.meta(tf.keras.models.Sequential)
+Dense = mpy.meta(tf.keras.layers.Dense)
+Lambda = mpy.meta(tf.keras.layers.Lambda)
+
+# meta program
+model = Sequential([
+    tf.keras.layers.Flatten(input_shape=(28, 28)),
+    *[mpy.List([
+        Lambda(lambda x: x), 
+        Dense(mpy.Int(8, 32), activation="relu")
+        ]) for _ in range(1)
+    ],
+    tf.keras.layers.Dense(10)
+])
+
+# search space
+choices = model.choice()
+
+rng = np.random.RandomState(42)
+
+for choice, model in mpy.sample_programs(5, model, rng):
+    print(" ** Sampling new model: ")
+    print("    - choice: ", choice, end="\n\n")
+    model = model.evaluate()
+    model.summary()
+    print("\n"*3)
