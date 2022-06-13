aa3c29449d0182686eef16969c6a630315bd821c
Deathn0t <romainegele@gmail.com>
2022-06-13 16:45:59 +0200

    adding twine to setup

---
+++ metalgpy/__version__.py
@@ -0,0 +1,6 @@
+VERSION = (0, 4, 0)
+
+__version__ = ".".join(map(str, VERSION))
+
+# alpha/beta/rc tags
+__version_suffix__ = ""
--- setup.py
+++ setup.py
@@ -1,4 +1,11 @@
-from setuptools import setup, find_packages
+import os
+import sys
+
+from setuptools import Command, find_packages, setup
+from shutil import rmtree
+
+# path of the directory where this file is located
+here = os.path.abspath(os.path.dirname(__file__))
 
 # What packages are required for this module to be executed?
 REQUIRED = [
@@ -7,6 +14,106 @@ REQUIRED = [
     "scipy",
 ]
 
+EXTRAS = {"dev": ["twine", "black"]}
 
 
-setup(name="metalgpy", packages=find_packages(), install_requires=REQUIRED)
+class UploadCommand(Command):
+    """Support setup.py upload."""
+
+    description = "Build and publish the package."
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print("\033[1m{0}\033[0m".format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status("Removing previous builds…")
+            rmtree(os.path.join(here, "dist"))
+        except OSError:
+            pass
+
+        self.status("Building Source and Wheel (universal) distribution…")
+        os.system("{0} setup.py sdist bdist_wheel --universal".format(sys.executable))
+
+        self.status("Uploading the package to PyPI via Twine…")
+        os.system("twine upload dist/*")
+
+        sys.exit()
+
+
+class TestUploadCommand(Command):
+    """Support setup.py testupload."""
+
+    description = "Build and publish the package to test.pypi."
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print("\033[1m{0}\033[0m".format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status("Removing previous builds…")
+            rmtree(os.path.join(here, "dist"))
+        except OSError:
+            pass
+
+        self.status("Building Source and Wheel (universal) distribution…")
+        os.system("{0} setup.py sdist bdist_wheel --universal".format(sys.executable))
+
+        self.status("Uploading the package to PyPI via Twine…")
+        os.system("twine upload --repository-url https://test.pypi.org/legacy/ dist/*")
+
+        sys.exit()
+
+
+class TestInstallCommand(Command):
+    """Support setup.py testinstall"""
+
+    description = "Install metalgpy from TestPyPI."
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print("\033[1m{0}\033[0m".format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        self.status("Downloading the package from Test PyPI and installing it")
+        os.system("pip install --index-url https://test.pypi.org/simple/ metalgpy")
+
+        sys.exit()
+
+
+# Where the magic happens:
+setup(
+    install_requires=REQUIRED,
+    extras_require=EXTRAS,
+    cmdclass={
+        "upload": UploadCommand,
+        "testupload": TestUploadCommand,
+        "testinstall": TestInstallCommand,
+    },
+)
